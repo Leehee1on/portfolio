fecf6bde17af1813f900dd6a2e480c22f19637ff
Heewon Lee <leehee@encoredtech.com>
2024-03-05 17:02:36 +0900

    update

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import '../assets/css/projectCard.css';
+import React from "react";
+import "../assets/css/projectCard.css";
 
 function ProjectCard({ project }: Props) {
-    const Fade = require('react-reveal/Fade');
-    return (
-        <Fade duration={800} bottom>
-        <a className="project-card-main" href={project.url} target="_blank">
-            <div className="card-title">
-                <p>{project.title}</p>
-            </div>
-            <div className="card-desc">
-                <p>{project.desc}</p>
-            </div>
-        </a>
-        </Fade>
-    );
+  const Fade = require("react-reveal/Fade");
+  return (
+    <Fade duration={800} bottom>
+      <a className="project-card-main" href={project.url} target="_blank" rel="noreferrer">
+        <div className="card-title">
+          <p>{project.title}</p>
+        </div>
+        <div className="card-desc">
+          <p>{project.desc}</p>
+        </div>
+      </a>
+    </Fade>
+  );
 }
 interface Props {
-    project: {
-        title: string,
-        desc: string,
-        url: string
-    }
+  project: {
+    title: string;
+    desc: string;
+    url: string;
+  };
 }
 export default ProjectCard;
